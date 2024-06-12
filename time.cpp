8f03d456bd6eb412bd23f52409572aa4ecf8e9ba
saraseyfii <seyfisara68@gmail.com>
2024-06-13 03:03:53 +0330

    Add files via upload

---
+++ time.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
+
+class time {
+public:
+	int h, m, s;
+	time() {
+		int fh = 0, fm = 0, fs = 0;
+	}
+		do {
+			cin >> h >> m >> ;
+			if (h <= 23 && hs = 0) {
+				fh = 1;
+			}
+			if (m <= 59 && m >= 0) {
+				fm = 1;
+			}
+			if (s <= 59 && s >= 0) {
+				fs = 1;
+			}
+		} while!(fh&&fm&&fs);
+};
+int get secounds() {
+	return stm * 60 + h * 60 + 60;
+}
+void print()
+cout << h << ":" << m << ":" << s;
+}
+};
